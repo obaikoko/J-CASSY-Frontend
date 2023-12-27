a15e1412f6f63d66f114d64029bffb38f1d23b51
obaikoko <jesseobinna7@gmail.com>
2023-12-27 03:25:29 +0100

    another commit

--- pages/Showcase.jsx
+++ pages/Showcase.jsx
@@ -7,51 +7,100 @@ function Showcase() {
     <>
       <section className={`${style.sections} ${style.sectionA}`}>
         <div className={style.sectionInner}>
-          <h4>J cassy Shoes</h4>
-          <h2>Collection</h2>
+          <h4>Discount</h4>
+          <h2>33% off on every purchase</h2>
           <Link className={style.btn} href='#'>
             <div className={style.hover}></div>
-            <span>view shoes</span>
+            <span>view gallery</span>
           </Link>
+        </div>
+      </section>
+      <section className={style.newArrivalsSection}>
+        <h1>New Arrivals</h1>
+        <div className={style.newArrivalsContainer}>
+          <div className={style.newArrival}>
+            <div className={style.newArrivalImg}>
+              <img
+                src='http://res.cloudinary.com/dzajrh9z7/image/upload/v1703634625/J-Cassy/zxamzmwpvkwj3eflmlnw.jpg'
+                alt=''
+              />
+            </div>
+            <Link className={style.link} href=''>
+              view
+            </Link>
+          </div>
+          <div className={style.newArrival}>
+            <div className={style.newArrivalImg}>
+              <img
+                src='http://res.cloudinary.com/dzajrh9z7/image/upload/v1703633412/J-Cassy/skoinfk5hcuqn7hjumkw.jpg'
+                alt=''
+              />
+            </div>
+            <Link className={style.link} href=''>
+              view
+            </Link>
+          </div>
+          <div className={style.newArrival}>
+            <div className={style.newArrivalImg}>
+              <img
+                src='http://res.cloudinary.com/dzajrh9z7/image/upload/v1703633023/J-Cassy/ltmjiweekcupmp7nom49.jpg'
+                alt=''
+              />
+            </div>
+            <Link className={style.link} href=''>
+              view
+            </Link>
+          </div>
+          <div className={style.newArrival}>
+            <div className={style.newArrivalImg}>
+              <img
+                src='http://res.cloudinary.com/dzajrh9z7/image/upload/v1703633252/J-Cassy/n9cxw9eqfu7q2sktasiv.jpg'
+                alt=''
+              />
+            </div>
+            <Link className={style.link} href=''>
+              view
+            </Link>
+          </div>
         </div>
       </section>
       <section className={`${style.sections} ${style.sectionC}`}>
         <div className={style.sectionInner}>
-          <h4>J cassy bags</h4>
-          <h2>Collection</h2>
+          <h4>locally produced</h4>
+          <h2>Best Quality hand made products</h2>
           <Link className={style.btn} href='#'>
             <div className={style.hover}></div>
-            <span>view bags</span>
+            <span>view hand made products</span>
           </Link>
         </div>
       </section>
       <section className={`${style.sections} ${style.sectionB}`}>
         <div className={style.sectionInner}>
-          <h4>J cassy bags</h4>
-          <h2>Collection</h2>
+          <h4>Pure leather</h4>
+          <h2>Crafted from best quality leather</h2>
           <Link className={style.btn} href='#'>
             <div className={style.hover}></div>
-            <span>view bags</span>
+            <span>view leather materials</span>
           </Link>
         </div>
       </section>
       <section className={`${style.sections} ${style.sectionE}`}>
         <div className={style.sectionInner}>
-          <h4>J cassy shirts</h4>
-          <h2>Collection</h2>
+          <h4>Men clothings</h4>
+          <h2>All kinds of wears for men</h2>
           <Link className={style.btn} href='#'>
             <div className={style.hover}></div>
-            <span>view shirts</span>
+            <span>view men wears</span>
           </Link>
         </div>
       </section>
       <section className={`${style.sections} ${style.sectionD}`}>
         <div className={style.sectionInner}>
-          <h4>J cassy bags</h4>
-          <h2>Collection</h2>
+          <h4>carton materials</h4>
+          <h2>made from carton</h2>
           <Link className={style.btn} href='#'>
             <div className={style.hover}></div>
-            <span>view bags</span>
+            <span>carton products</span>
           </Link>
         </div>
       </section>
