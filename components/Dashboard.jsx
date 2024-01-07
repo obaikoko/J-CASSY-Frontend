3d9b2cf3028ee0aca7ffdabf830becaab45ab2d4
obaikoko <jesseobinna7@gmail.com>
2024-01-07 18:09:59 +0100

    updated Dashboard

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -15,28 +15,52 @@ import {
   FaCartArrowDown,
   FaShoppingBag,
   FaSignOutAlt,
+  FaSun,
+  FaBell,
+  FaUserAlt,
+  FaUserLock,
+  FaLaptop,
+  FaShoppingBasket,
+  FaTv,
+  FaLaptopCode,
+  FaSearch,
+  FaDotCircle,
 } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { logout, reset } from '@/src/features/auth/authSlice';
 import BarChart from './BarChart';
 
 function Dashboard() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-
-      const onClick = () => {
-        dispatch(logout());
-        dispatch(reset());
-        window.location.reload();
-      };
+  const onClick = () => {
+    dispatch(logout());
+    dispatch(reset());
+    window.location.reload();
+  };
   return (
     <>
-      ;
       <div className={style.container}>
         <aside className={style.aside}>
           <ul>
-            <li className={style.active}>Dashboard</li>
-            <li>Products</li>
+            <li>
+              <div className={style.icon}>
+                <FaLaptopCode />
+              </div>
+              Administrator
+            </li>
+            <li className={style.active}>
+              <div className={style.icon}>
+                <FaTv />
+              </div>
+              Dashboard
+            </li>
+            <li>
+              <div className={style.icon}>
+                <FaShoppingBasket />
+              </div>
+              Products
+            </li>
             <li>
               <div className={style.icon}>
                 <FaShoppingCart />
@@ -102,50 +126,150 @@ function Dashboard() {
           </ul>
         </aside>
         <main className={style.main}>
-          <h2>Dashboard</h2>
-          <ul>
-            <li>
-              <div className={style.icon}>
-                <h1>
-                  <FaFileInvoiceDollar />
-                </h1>
-              </div>
-              <div className={style.text}>
-                <small>Total Sales </small>
-                <h4>$176,727,210.46</h4>
-              </div>
-            </li>
-            <li>
-              <div className={style.icon}>
-                <h1>
-                  <FaCartArrowDown />
-                </h1>
-              </div>
-              <div className={style.text}>
-                <small>Total Orders </small>
-                <h4>12,892</h4>
-              </div>
-            </li>
-            <li>
-              <div className={style.icon}>
-                <h1>
-                  <FaShoppingBag />
-                </h1>
-              </div>
-              <div className={style.text}>
-                <small>Total Products </small>
-                <h4>1,588</h4>
-              </div>
-            </li>
-          </ul>
-          <div className={style.chart}>
-            <BarChart />
-          </div>
-
-          <div className={style.orders}>
+          {/* <header className={style.header}>
+            <form className={style.form}>
+              <input type='text' placeholder='Search' />
+            </form>
             <ul>
-              <li></li>
+              <li>
+                <button>
+                  <h2>
+                    <FaSun />
+                  </h2>
+                </button>
+              </li>
+              <li>
+                <button>
+                  <h2>
+                    <FaBell />
+                  </h2>
+                </button>
+              </li>
+              <li>
+                <button>
+                  <div className={style.imgContainer}>
+                    <img
+                      src='https://randomuser.me/api/portraits/women/7.jpg'
+                      alt='name'
+                    />
+                  </div>
+                </button>
+              </li>
             </ul>
+          </header> */}
+          <div className={style.dashboardBody}>
+            <h2>Dashboard</h2>
+            <ul>
+              <li>
+                <div className={style.icon}>
+                  <h1>
+                    <FaFileInvoiceDollar />
+                  </h1>
+                </div>
+                <div className={style.text}>
+                  <small>Total Sales </small>
+                  <h4>$176,727,210.46</h4>
+                </div>
+              </li>
+              <li>
+                <div className={style.icon}>
+                  <h1>
+                    <FaCartArrowDown />
+                  </h1>
+                </div>
+                <div className={style.text}>
+                  <small>Total Orders </small>
+                  <h4>12,892</h4>
+                </div>
+              </li>
+              <li>
+                <div className={style.icon}>
+                  <h1>
+                    <FaShoppingBag />
+                  </h1>
+                </div>
+                <div className={style.text}>
+                  <small>Total Products </small>
+                  <h4>1,588</h4>
+                </div>
+              </li>
+            </ul>
+            <div className={style.chart}>
+              <BarChart />
+            </div>
+          </div>
+          <div className={style.orders}>
+            <h2>
+                Latest Orders
+            </h2>
+            <div className={style.orderInfo}>
+              <div className={style.userId}>
+                <ul>
+                  <li>s/n</li>
+                  <li>name</li>
+                </ul>
+              </div>
+              <div className={style.userDetails}>
+                <ul>
+                  <li>Email</li>
+                  <li>Amount</li>
+                  <li>Status</li>
+                  <li>Date</li>
+                  <li>...</li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.orderInfo}>
+              <div className={style.userId}>
+                <ul>
+                  <li>s/n</li>
+                  <li>name</li>
+                </ul>
+              </div>
+              <div className={style.userDetails}>
+                <ul>
+                  <li>Email</li>
+                  <li>Amount</li>
+                  <li>Status</li>
+                  <li>Date</li>
+                  <li>...</li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.orderInfo}>
+              <div className={style.userId}>
+                <ul>
+                  <li>s/n</li>
+                  <li>name</li>
+                </ul>
+              </div>
+              <div className={style.userDetails}>
+                <ul>
+                  <li>Email</li>
+                  <li>Amount</li>
+                  <li>Status</li>
+                  <li>Date</li>
+                  <li>...</li>
+                </ul>
+              </div>
+            </div>
+            <div className={style.orderInfo}>
+              <div className={style.userId}>
+                <ul>
+                  <li>s/n</li>
+                  <li>name</li>
+                </ul>
+              </div>
+              <div className={style.userDetails}>
+                <ul>
+                  <li>Email</li>
+                  <li>Amount</li>
+                  <li>Status</li>
+                  <li>Date</li>
+                  <li>...</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </main>
       </div>
