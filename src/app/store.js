1de6995fff39b49b60b15fbcfcd753436a86eeaa
obaikoko <jesseobinna7@gmail.com>
2024-06-08 13:37:27 +0100

    updated

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
-import productReducer from '../features/products/productSlice';
-import cartReducer from '../features/cart/cartSlice'
+// import { configureStore } from '@reduxjs/toolkit';
+// import authReducer from '../features/auth/authSlice';
+// import productReducer from '../features/products/productSlice';
+// import cartReducer from '../features/cart/cartSlice'
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    products: productReducer,
-    cart: cartReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     products: productReducer,
+//     cart: cartReducer,
+//   },
+// });
